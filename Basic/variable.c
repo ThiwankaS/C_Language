7c620f2b4e1f3637fcbeb0d4d932ddc9b232befa
Thiwanka Somachandra <thivankas@outlook.com>
2024-07-12 21:08:28 +0300

    Basic data types and variables

---
+++ Basic/variable.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (void) {
+    //Varibale declartion and value assiging inline
+    int position = 10;
+    printf("Position : %d\n ",position);
+    //Varibale declartion 
+    int x, y, z;
+    //Value assiging inline
+    x = 2;
+    y = 3;
+    z = 4;
+    printf("x : %d, y : %d, z = %d \n",x,y,z);
+    //Numbers with decimal value with out high precesion
+    float mynum = 98.125678; 
+    printf("mynum : %lf\n",mynum);
+    //Numbers with decimal value with high precesion
+    double mydouble = 98.125678;
+    printf("mydouble : %lf\n",mydouble);
+    //Character declaratio and value assigning
+    char c = 'Y';
+    printf("c : %c\n",c);
+    //Printing ASCII value 
+    printf("c : %d\n",c);
+    return 0;
+}
