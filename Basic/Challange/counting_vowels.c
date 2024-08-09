a7c33db95f5f4f91d17e2fc38c1363567ea15e29
Thiwanka Somachandra <thivankas@outlook.com>
2024-08-09 05:01:31 +0300

    Counting no of vowels in a string - Refactoring

--- Basic/Challange/counting_vowels.c
+++ Basic/Challange/counting_vowels.c
@@ -2,14 +2,26 @@
 #include <string.h>
 #include <ctype.h>
 
+int vowel_count(char *s);
+
 int main(void)
 {
-    char str[] = "It's a wonderful life!";
-    int length = strlen(str);
+    char str1[] = "It's a wonderful life!";
+    char str2[] = "AaEeIiOoUu";
+    int str1Count = vowel_count(str1);
+    int str2Count = vowel_count(str2);
+    printf("No of vowels in the string 1 is : %d\n", str1Count);
+    printf("No of vowels in the string 2 is : %d\n", str2Count);
+    return 0;
+}
+
+int vowel_count(char *s)
+{
+    int length = strlen(s);
     int count = 0;
     for (int i = 0; i < length; i++)
     {
-        switch (tolower(str[i]))
+        switch (tolower(s[i]))
         {
         case 'a':
         case 'e':
@@ -19,7 +31,5 @@ int main(void)
             count++;
         }
     }
-    printf("No of vowels in the string is : %d", count);
-    printf("\n");
-    return 0;
+    return count;
 }
